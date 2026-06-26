6e18e754cab73ce5ee05dd8ea2d2d4fb9c84d82e
Mina <menady20@gmail.com>
2026-06-26 15:14:01 +0300

    Complete getHumanChoice function

--- script.js
+++ script.js
@@ -15,3 +15,8 @@ function getComputerChoice() {
             break;
     }    
 }
+function getHumanChoice() {
+    let humanChoice = prompt("Enter Your Choice: ")
+    console.log(humanChoice);
+    
+}
