045a6934bf620b89db467a312dfa3e9f3d001672
Mina <menady20@gmail.com>
2026-06-26 15:11:51 +0300

    Complete getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    let random = Math.floor(Math.random() * 3);
+    switch (random) {
+        case 0:
+            console.log("Rock");
+            break;
+        case 1:
+            console.log("Paper");
+            break;
+        case 2:
+            console.log("Scissors");
+            break;
+        default:
+            console.log("Error");
+            break;
+    }    
+}
